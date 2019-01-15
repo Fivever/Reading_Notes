265cff5962e410b6e05b1b90a6f2e173725033fd
Jason Xiong <37428285+Fivever@users.noreply.github.com>
2019-01-15 22:30:41 +0800

    Update SeqStack.cpp

--- Data_Structure/SeqStack.cpp
+++ Data_Structure/SeqStack.cpp
@@ -2,51 +2,59 @@
 #include <cstdlib>
 
 typedef int ElementType;
-#define StackSize 100
+#define STACKSIZE 100
+#define STACKINCREMENT 10
 
 typedef struct
 {
-    ElementType* data;
-    int top;
+    ElementType *top, *base;
+    int StackSize;
 }SeqStack;
 
 void InitStack(SeqStack &S);//Initialize the sequence stack
-void Push(SeqStack &S, ElementType x);//push a value to the top of stack
-void Pop(SeqStack &S);//delete the top value of stack
+bool Push(SeqStack &S, ElementType x);//push a value to the top of stack
+bool Pop(SeqStack &S);//delete the top value of stack
 ElementType GetTop(SeqStack S);//return the top value of stack
 void Print(SeqStack S);//print all the elements of stack
 
 void InitStack(SeqStack &S)
 {//Initialize the sequence stack
-    S.data=(ElementType*)malloc(StackSize*sizeof(ElementType));
-    S.top=-1;
+    S.base=(ElementType*)malloc(STACKSIZE*sizeof(ElementType));
+    S.top=S.base;
+    S.StackSize = STACKSIZE;
 }
 
-void Push(SeqStack &S, ElementType x)
+bool Push(SeqStack &S, ElementType x)
 {//push a value to the top of stack
-    if(S.top<StackSize)
+    if (S.top-S.base>=S.StackSize)
     {
-        S.top++;
-        S.data[S.top]=x;
+        S.base=(ElementType*)realloc(S.base,(S.StackSize+STACKINCREMENT)*sizeof(ElementType));  
+        if(!S.base) exit(-1);
+        S.top=S.base+S.StackSize;
+        S.StackSize+=STACKINCREMENT;
     }
+    *S.top++=x;
+    return true;
 }
 
-void Pop(SeqStack &S)
+bool Pop(SeqStack &S)
 {//delete the top value of stack
-    if(S.top>0)
-        S.top--;
+    if (S.top == S.base)
+        return false;  
+    --S.top;
+    return true;
 }
 
 ElementType GetTop(SeqStack S)
 {//return the top value of stack
-    return S.data[S.top];
+    return *(S.top-1);
 }
 
 void Print(SeqStack S)
 {//print all the elements of stack
-    for(int i=S.top;i>0;i--)
-        printf("%d ",S.data[i]);
-    printf("%d\n",S.data[0]);
+    for(ElementType *i=S.top-1; i!=S.base; i--)
+        printf("%d ",*i);
+    printf("%d\n",*S.base);
 }
 
 int main()
